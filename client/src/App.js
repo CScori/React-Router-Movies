53037cf8250ab165068bb3b96ff1acd8f9746a32
CScori <amilanotte1366@gmail.com>
2019-08-07 21:08:56 -0500

    adds router and paths and links

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
-
+import { Route, Link } from "react-router-dom";
 import SavedList from './Movies/SavedList';
+import Movie from "./Movies/Movie";
+import MovieList from "./Movies/MovieList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
+
+      <Link to="/">Home</Link>
+      <Link to="/movies/">Movies</Link>
+
+      <Route path="/" component={MovieList} />
+      <Route path="/movies/:id" component={Movie} />
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
     </div>
